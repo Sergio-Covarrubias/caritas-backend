2cad08b3d7c1cbb3985143d8ac3a823531dae456
Desisacc <saccescuela@gmail.com>
2025-09-27 19:04:47 -0600

    C4 - Added pagination to hostels
    Modified the GET /hostel endpoint so it can receive query parameters, incluiding one that allows filtering according to the services that the hostel provides. It also has query parameters to search between a time period while also returning the available number of spaces for each hostel.
    Fun fact: This is the first time I do filtering on a many-to-many relation, the query looks quite scary, thanks to chatgpt for existing! Let's just hope the examiner doesn't read this commit comment though...

--- src/main/java/com/caritas/backend/core/hostels/HostelController.java
+++ src/main/java/com/caritas/backend/core/hostels/HostelController.java
@@ -1,5 +1,7 @@
 package com.caritas.backend.core.hostels;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.caritas.backend.core.hostels.dtos.HostelPaginationResponse;
 import com.caritas.backend.core.hostels.dtos.HostelRequest;
 import com.caritas.backend.core.hostels.dtos.HostelResponse;
 
@@ -26,8 +30,20 @@ public class HostelController {
     }
 
     @GetMapping
-    public List<HostelResponse> getAllHostels() {
-        return hostelService.getAllHostels();
+    public List<HostelPaginationResponse> getAvailableHostels(
+            @RequestParam(defaultValue = "5") int limit,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(required = false) List<String> filters,
+            @RequestParam String startDate,
+            @RequestParam String endDate) {
+        if (filters == null) {
+            filters = List.of();
+        }
+
+        LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        return hostelService.getPaginatedHostels(limit, page, filters, start, end);
     }
 
     @GetMapping("/{id}")
