b4e3f9228ede3573bea1d6879c97d5bf338fe5bb
Desisacc <saccescuela@gmail.com>
2025-10-02 22:20:26 -0600

    C6 - Added the reservation POST endpoint
    Added the POST endpoint for the reservations which also creates the service_interests and person_reservations.
    Improved the Python script that fills the database data by adding random data generation to make the placeholder data feel more natural.
    Added more information to the README in regards to the database container.
    Fun fact: This commit took me a long time because adding the table person_reservations made more changes to the application than I thought. The content of this commit isn't even that much. And I still want to do some CI/CD stuff, let's see if I have enough time (at least CD).

--- src/main/java/com/caritas/backend/core/reservations/ReservationService.java
+++ src/main/java/com/caritas/backend/core/reservations/ReservationService.java
@@ -1,5 +1,6 @@
 package com.caritas.backend.core.reservations;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,12 +8,24 @@ import org.springframework.stereotype.Service;
 
 import com.caritas.backend.core.hostels.entities.HostelEntity;
 import com.caritas.backend.core.hostels.entities.HostelRepository;
+import com.caritas.backend.core.person_reservations.entities.PersonReservationEntity;
+import com.caritas.backend.core.person_reservations.entities.PersonReservationRepository;
+import com.caritas.backend.core.persons.entities.PersonEntity;
+import com.caritas.backend.core.persons.entities.PersonRepository;
+import com.caritas.backend.core.reservations.dtos.CreateReservationRequest;
+import com.caritas.backend.core.reservations.dtos.CreateReservationResponse;
 import com.caritas.backend.core.reservations.dtos.ReservationRequest;
 import com.caritas.backend.core.reservations.dtos.ReservationResponse;
 import com.caritas.backend.core.reservations.entities.ReservationEntity;
 import com.caritas.backend.core.reservations.entities.ReservationRepository;
+import com.caritas.backend.core.service_interests.entities.ServiceInterestEntity;
+import com.caritas.backend.core.service_interests.entities.ServiceInterestRepository;
+import com.caritas.backend.core.services.entities.ServiceEntity;
+import com.caritas.backend.core.services.entities.ServiceRepository;
 import com.caritas.backend.core.users.entities.UserEntity;
 import com.caritas.backend.core.users.entities.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ReservationService {
@@ -20,12 +33,22 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
     private final HostelRepository hostelRepository;
+    private final PersonRepository personRepository;
+    private final ServiceRepository serviceRepository;
+    private final PersonReservationRepository personReservationRepository;
+    private final ServiceInterestRepository serviceInterestRepository;
 
     public ReservationService(ReservationRepository reservationRepository, UserRepository userRepository,
-            HostelRepository hostelRepository) {
+            HostelRepository hostelRepository, PersonRepository personRepository, ServiceRepository serviceRepository,
+            PersonReservationRepository personReservationRepository,
+            ServiceInterestRepository serviceInterestRepository) {
         this.reservationRepository = reservationRepository;
         this.userRepository = userRepository;
         this.hostelRepository = hostelRepository;
+        this.personRepository = personRepository;
+        this.serviceRepository = serviceRepository;
+        this.personReservationRepository = personReservationRepository;
+        this.serviceInterestRepository = serviceInterestRepository;
     }
 
     public List<ReservationResponse> getAllReservations() {
@@ -41,14 +64,33 @@ public class ReservationService {
         return new ReservationResponse(reservation);
     }
 
-    public ReservationResponse createReservation(ReservationRequest request) {
+    @Transactional
+    public CreateReservationResponse createReservation(CreateReservationRequest request) {
+        boolean hasActiveReservation = reservationRepository.existsByUserIdAndActiveTrue(request.userId());
+        if (hasActiveReservation) {
+            throw new IllegalStateException("User already has an active reservation");
+        }
+
         UserEntity user = userRepository.findOneOrFail(request.userId());
         HostelEntity hostel = hostelRepository.findOneOrFail(request.hostelId());
 
-        ReservationEntity reservation = new ReservationEntity(user, hostel, request.startDate(), request.endDate());
-        ReservationEntity saved = reservationRepository.save(reservation);
+        PersonEntity[] persons = Arrays.stream(request.personIds())
+                .map(personRepository::findOneOrFail).toArray(PersonEntity[]::new);
+        ServiceEntity[] services = Arrays.stream(request.serviceIds())
+                .map(serviceRepository::findOneOrFail).toArray(ServiceEntity[]::new);
 
-        return new ReservationResponse(saved, user.getId(), hostel.getId());
+        ReservationEntity reservation = reservationRepository
+                .save(new ReservationEntity(user, hostel, request.startDate(), request.endDate()));
+
+        UUID[] personReservationIds = Arrays.stream(persons).map(
+                person -> personReservationRepository.save(new PersonReservationEntity(person, reservation)).getId())
+                .toArray(UUID[]::new);
+        UUID[] serviceInterestIds = Arrays.stream(services)
+                .map(service -> serviceInterestRepository.save(new ServiceInterestEntity(reservation, service)).getId())
+                .toArray(UUID[]::new);
+
+        return new CreateReservationResponse(reservation.getId(), user.getId(), hostel.getId(),
+                reservation.getStartDate(), reservation.getEndDate(), personReservationIds, serviceInterestIds);
     }
 
     public ReservationResponse updateReservation(UUID id, ReservationRequest request) {
---
+++ src/main/java/com/caritas/backend/core/reservations/dtos/CreateReservationRequest.java
@@ -0,0 +1,17 @@
+package com.caritas.backend.core.reservations.dtos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public record CreateReservationRequest(
+        @NotBlank String userId,
+        @NonNull UUID hostelId,
+        @NonNull LocalDate startDate,
+        @NonNull LocalDate endDate,
+        @NotEmpty UUID[] personIds,
+        @NonNull UUID[] serviceIds) {
+}
